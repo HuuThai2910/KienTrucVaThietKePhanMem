67a35fb7f39e71413b01b3fa0f96c6b7905f2abe
HuuThai <levanhuuthai2004@gmail.com>
2026-05-11 11:36:41 +0700

    add lab 8, 9

---
+++ lab7/food_order_serivce_base/frontend/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiGatewayUrl = 'http://192.168.1.29:8085'
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+
+    server: {
+      host: '0.0.0.0', // 🔥 QUAN TRỌNG
+      port: 3000,
+
+      proxy: {
+        '/api': {
+          target: apiGatewayUrl,
+          changeOrigin: true,
+        },
+        '/foods': {
+          target: apiGatewayUrl,
+          changeOrigin: true,
+        },
+        '/orders': {
+          target: apiGatewayUrl,
+          changeOrigin: true,
+        },
+        '/payments': {
+          target: apiGatewayUrl,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
+})
